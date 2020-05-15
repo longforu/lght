abe083ebb496f0c8a8ea9547aa4335c018403d33
VietLongTran <58486890+VietLongTran@users.noreply.github.com>
2020-05-14 19:21:51 -0700

    Fix

--- lght.js
+++ lght.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 //LIGHT LIBRARY IS DEVELOPED BY LONG TRAN
 //COPYRIGHT 2020, ALL RIGHTS RESERVED
-//Test stuff
+//Test stuff 2
 
 import {getByDotNotation, changeByDotNotation , mergeDefaultPropertyObject, findDistance2Point, findIntersection, rotatePoint, degToRad, findAngle2Point ,findClosestPoint, calculateRotatePoint, radToDeg} from './util/util'
 
