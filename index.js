4836f794afffb526311168adee695b106c5659f6
VietLongTran <58486890+VietLongTran@users.noreply.github.com>
2020-05-16 14:27:33 -0700

    Fix

--- index.js
+++ index.js
@@ -195,6 +195,8 @@ lght.element = (_temp3 = class extends lght.coordinate {
     mergeDefaultPropertyObject(option, config.defaultDomConfig, this);
     this.element = document.createElement(this.type);
     this.parent = parent;
+    this.style = {};
+    mergeDefaultPropertyObject(option.style, config.defaultStyleConfig, this.style);
     this.parseStyle();
     this.parent.divElement.appendChild(this.element);
   }
